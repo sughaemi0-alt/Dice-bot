d0380e30b1bdfef084f4c6940a853682a92662b1
sughaemi0-alt <sughaemi0@gmail.com>
2026-09-13 03:24:16 +0900

    Update Main.py

--- Main.py
+++ Main.py
@@ -10,7 +10,7 @@ app = Flask('')
 
 @app.route('/')
 def home():
-    return "Online-Dice Analytics Engine is Live!"
+    return "Online-Dice Analytics Engine is Live!" 
 
 def run():
     app.run(host='0.0.0.0', port=8080)
